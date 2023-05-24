d7e5e862314713f1de8b7f0b9306bd81ae049c63
WengangXbio <85557809+WengangXbio@users.noreply.github.com>
2023-05-24 11:56:24 +0100

    Update pca_plot.r

--- pca_plot.r
+++ pca_plot.r
@@ -1,5 +1,6 @@
 .libPaths('~/schoenebeck_group/WENGANG/R_lib/') #the path of the R package should be modified accordingly
 library(ggplot2) 
+library(gridExtra)
 args <- commandArgs(T)
 df= read.table(args[1], sep=" ", stringsAsFactors=FALSE, header=F)
 plot1 <- ggplot(df, aes(x = V3, y = V4)) + geom_point() + labs(title = "PCA Plot", x = "PC1", y = "PC2")
