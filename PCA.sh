9cad5baeb72c736e226f33eff8b62ca045672059
WengangXbio <85557809+WengangXbio@users.noreply.github.com>
2023-05-25 13:30:52 +0100

    Update PCA.sh

--- PCA.sh
+++ PCA.sh
@@ -1,6 +1,7 @@
 awk '$1>38 {print}' $1.bim > non-auto.bim
 plink --bfile $1 --exclude non-auto.bim --make-bed --out temp1 --chr-set 42
-plink --bfile temp1 --indep-pairwise 100 20 $2 --chr-set 42 --out ld
-plink --bfile temp1 --exclude ld.prune.out --make-bed --out temp2 --chr-set 42
-plink --bfile temp2 --pca --out $1 --chr-set 42
+plink --bfile temp1 --hwe $2 --make-bed --out temp2 --chr-set 42
+plink --bfile temp2 --indep-pairwise 100 20 $3 --chr-set 42 --out ld
+plink --bfile temp2 --exclude ld.prune.out --make-bed --out temp3 --chr-set 42
+plink --bfile temp3 --pca --out $1 --chr-set 42
 rm temp* ld* non-auto.bim
