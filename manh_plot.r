2aae7f9daa0e17c635997c2ae482a352d37b2093
WengangXbio <85557809+WengangXbio@users.noreply.github.com>
2023-05-29 16:18:30 +0100

    Update manh_plot.r

--- manh_plot.r
+++ manh_plot.r
@@ -1,4 +1,4 @@
-setwd("K:\\schoenebeck_group\\WZ\\dropbox")
+setwd("dropbox")
 trait='test'
 library(qqman)
 test=read.table(paste(trait,".mlma.manh",sep=""),head=T)
@@ -6,4 +6,7 @@ colnames(test)=c("SNP","CHR","BP","P")
 snpsOfInterest=test$SNP[which(test$P<=1e-08)]
 tiff(paste(trait,".mlma.manh.tif",sep=""),units="in", width=30, height=10, res=100)
 manhattan(test,suggestiveline = F,genomewideline = 8,highlight = snpsOfInterest)
+dev.off() 
+tiff(paste(paste(trait,".qqplot.tif",sep=""),sep=""),units="in", width=10, height=10, res=100)
+qq(test$P)
 dev.off() 
